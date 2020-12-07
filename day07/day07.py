88ee8a53f0cefa7b875a6d0033ce85825c0f081a
Markus Bauer <mkbauer@web.de>
2020-12-07 10:19:07 +0100

    Solution for day 7 part 2.

--- day07/day07.py
+++ day07/day07.py
@@ -4,26 +4,33 @@ import re
 import os
 import sys
 
-class Graph:
+class DirectedGraph:
 
     def __init__(self):
         self.nodes = {}
 
-    def add_edge(self, node1, node2):
+    def add_edge(self, node1, node2, weight=0):
         if node1 in self.nodes:
-            self.nodes[node1].append(node2)
+            self.nodes[node1].append((node2, weight))
         else:
-            self.nodes[node1] = [node2]
+            self.nodes[node1] = [(node2, weight)]
 
     def find_all_reachable_nodes(self, node, visited=set()):
         if not node in self.nodes:
             return visited
-        for next in self.nodes[node]:
+        for next in [ neighbour for (neighbour, weight) in self.nodes[node]]:
             if not next in visited: 
                 visited.add(next)
                 visited.union(self.find_all_reachable_nodes(next, visited))
         return visited
-        
+
+    def accumulate_weights(self, node):
+        if not node in self.nodes:
+            return 1
+        sum = 1
+        for (neighbour, weight) in self.nodes[node]:
+            sum = sum + weight * self.accumulate_weights(neighbour)
+        return sum
 
     # For debugging
     def __repr__(self):
@@ -31,8 +38,8 @@ class Graph:
         return str.format(nodes=self.nodes)
 
     
-def build_graph(input):
-    graph = Graph()
+def build_graph_part1(input):
+    graph = DirectedGraph()
     for line in input:
         words = line.split()
         outer_bag = words[0] + words [1]
@@ -41,22 +48,41 @@ def build_graph(input):
             number = words[0]
             if number != "no":
                 inner_bag = words[1] + words[2]
-                print("Adding edge (%s, %s)" % (inner_bag, outer_bag))
-                graph.add_edge(inner_bag, outer_bag)
+                # print("Adding edge (%s, %s)" % (inner_bag, outer_bag))
+                graph.add_edge(inner_bag, outer_bag, int(number))
                 words = words[4:]
             else:
                 words = []
-    print(graph)
+    # print(graph)
+    return graph
+
+def build_graph_part2(input):
+    graph = DirectedGraph()
+    for line in input:
+        words = line.split()
+        outer_bag = words[0] + words [1]
+        words = words[4:]
+        while len(words) > 0:
+            number = words[0]
+            if number != "no":
+                inner_bag = words[1] + words[2]
+                # print("Adding edge (%s, %s) with weight %s" % (inner_bag, outer_bag, number))
+                graph.add_edge(outer_bag, inner_bag, int(number))
+                words = words[4:]
+            else:
+                words = []
+    # print(graph)
     return graph
 
 
 def compute07(input):
-    graph = build_graph(input)
+    graph = build_graph_part1(input)
     result = len(graph.find_all_reachable_nodes("shinygold"))
     return result
 
 def compute07b(input):
-    result = 0
+    graph = build_graph_part2(input)
+    result = graph.accumulate_weights("shinygold")-1
     return result
 
 def main():    
--- day07/day07_test.py
+++ day07/day07_test.py
@@ -12,12 +12,33 @@ class Day07Test(unittest.TestCase):
         input_file = os.path.abspath(os.path.dirname(__file__)) + os.path.sep + "example07.txt"
         with open(input_file) as f:
             input = [x.strip() for x in f]
-        graph = day07.build_graph(input)
-        print(graph.find_all_reachable_nodes("shinygold"))
+        graph = day07.build_graph_part1(input)
+        # print(graph)
+        # print(graph.find_all_reachable_nodes("shinygold"))
+        self.assertEqual(len(graph.find_all_reachable_nodes("shinygold")),  4)
+
+    def test_compute07b_on_example_data(self):
+        input_file = os.path.abspath(os.path.dirname(__file__)) + os.path.sep + "example07.txt"
+        with open(input_file) as f:
+            input = [x.strip() for x in f]
+        graph = day07.build_graph_part2(input)
+        # print(graph)
+        graph = day07.build_graph_part2(input)
+        self.assertEqual(graph.accumulate_weights("shinygold")-1, 32)
 
     def test_compute07b(self):
-        input = [""]
-        self.assertEqual(day07.compute07b(input), 0)
+        input = [
+            "shiny gold bags contain 2 dark red bags.",
+            "dark red bags contain 2 dark orange bags.",
+            "dark orange bags contain 2 dark yellow bags.",
+            "dark yellow bags contain 2 dark green bags.",
+            "dark green bags contain 2 dark blue bags.",
+            "dark blue bags contain 2 dark violet bags.",
+            "dark violet bags contain no other bags."
+        ]
+        graph = day07.build_graph_part2(input)
+        self.assertEqual(graph.accumulate_weights("shinygold")-1, 126)
+
 
 if __name__ == "__main__": 
     unittest.main()
