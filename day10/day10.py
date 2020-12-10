4115c8179b1eafd908ef06ba78c90ae9b8526a01
Markus Bauer <mkbauer@web.de>
2020-12-10 22:26:23 +0100

    Initial solution for day 10.

--- day10/day10.py
+++ day10/day10.py
@@ -4,18 +4,93 @@ import re
 import os
 import sys
 
+from typing import List
+from typing import Tuple
+from typing import Dict
+
+# Convert input into list of ints
+def read_numbers(input: List[str]) -> List[int]:
+    result = [ int(line) for line in input ]
+    return result
+
+def find_differences_in_adapter_chain(adapters: List[int]) -> Tuple[int, int]:
+    adapters.sort()
+    i = 0
+    prev = 0
+    ones = 0
+    threes = 0
+    while i < len(adapters):
+        next = adapters[i]
+        if next - prev == 1:
+            ones += 1
+        else:
+            if next -prev == 3:
+                threes += 1
+            else:
+                raise ValueError("Difference != 0 or 3")
+        prev = next
+        i += 1
+    threes += 1
+    return (ones, threes)
+
+def find_adapter_chains(current: int, pos: int, adapters: List[int], chain : List[int]) -> List[List[int]]:
+    chains: List[List[int]] = []
+    chain.append(current) 
+    if pos == len(adapters):
+        return [chain]
+    while pos < len(adapters):
+        next = adapters[pos]
+        pos += 1
+        difference = next - current
+        if difference > 3:
+            break
+        if difference <= 3 :
+            chains = chains + (find_adapter_chains(next, pos, adapters, chain.copy()))
+        print(chains)
+    return chains
+
+def count_adapter_chains(start: int, adapters: List[int], memo: Dict[int, int]=None) -> int:
+    if memo == None:
+        memo = {}
+    current = adapters[start]
+    if start == len(adapters)-1:
+        return 1
+    if memo and start in memo:
+        return memo[start]
+    chains = 0
+    pos = start + 1 
+    while pos < len(adapters):
+        next = adapters[pos]
+        difference = next - current
+        if difference > 3:
+            break
+        if difference <= 3 :
+            chains = chains + (count_adapter_chains(pos, adapters, memo))
+        pos += 1
+    if memo:
+        memo[start] = chains    
+    return chains
+
 def part1(input):
-    result = 0
+    adapters = read_numbers(input)
+    (ones, threes) = find_differences_in_adapter_chain(adapters)
+    result = ones * threes
     return result
 
 def part2(input):
-    result = 0
-    return result
+    adapters = read_numbers(input)
+    adapters.append(0)
+    device = max(adapters) + 3
+    adapters.append(device)
+    adapters.sort()
+    print(adapters)
+    chains = count_adapter_chains(0, adapters)
+    return chains
 
 def main():    
 
     # Official input
-    input_file = os.path.abspath(os.path.dirname(__file__)) + os.path.sep + "inputXX.txt"
+    input_file = os.path.abspath(os.path.dirname(__file__)) + os.path.sep + "input10.txt"
     with open(input_file) as f:
         input = [l.strip() for l in f]
 
--- day10/day10_test.py
+++ day10/day10_test.py
@@ -8,15 +8,52 @@ import day10
 
 class Day10Test(unittest.TestCase):
 
-    def test_part1_on_example_data(self):
-        input_file = os.path.abspath(os.path.dirname(__file__)) + os.path.sep + "example10.txt"
+    def test_part1_on_example_a(self):
+        input_file = os.path.abspath(os.path.dirname(__file__)) + os.path.sep + "example10a.txt"
         with open(input_file) as f:
             input = [l.strip() for l in f]
-        self.assertEqual(day10.part1(input), 0)
+         
+        adapters = day10.read_numbers(input)
+        print(adapters)
+        (ones, threes) = day10.find_differences_in_adapter_chain(adapters)
+        self.assertEqual((ones, threes), (7, 5))
 
-    def test_part2(self):
-        input = [""]
-        self.assertEqual(day10.part2(input), 0)
+    def test_find_adapter_chains(self):
+        self.assertEqual(day10.find_adapter_chains(0, 0, [3], []), [[0, 3]])
+        self.assertEqual(day10.find_adapter_chains(0, 0, [1, 4], []), [[0, 1, 4]])
+        self.assertEqual(day10.find_adapter_chains(0, 0, [1, 3, 4], []), [[0, 1, 3, 4], [0, 1, 4], [0, 3, 4]])
+        print(day10.find_adapter_chains(0, 0, [1, 4, 5, 6, 7, 10], []))
+        print(day10.find_adapter_chains(0, 0, [1, 4, 5, 6, 7, 10, 11, 12, 13], []))
+        print(day10.find_adapter_chains(0, 0, [1, 4, 5, 6, 7, 10, 11, 13], []))
+
+        # print(day10.find_adapter_chains(0, 0, [1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, 22], []))
+        # self.assertEqual(day10.find_adapter_chains(0, [1, 4, 5, 6, 7], []), [[0, 1, 4, 5, 7], [0, 1, 4, 6, 7]])
+
+    def test_find_adapter_chains_on_example(self):
+        input_file = os.path.abspath(os.path.dirname(__file__)) + os.path.sep + "example10a.txt"
+        with open(input_file) as f:
+            input = [l.strip() for l in f]
+
+        adapters = day10.read_numbers(input)
+        adapters.append(22)
+        adapters.sort()
+        chains = day10.find_adapter_chains(0, 0, adapters, [])
+        print(chains)
+        self.assertEquals(len(chains), 8)
+
+    def test_part2_on_example_a(self):
+        input_file = os.path.abspath(os.path.dirname(__file__)) + os.path.sep + "example10a.txt"
+        with open(input_file) as f:
+            input = [l.strip() for l in f]
+        
+        self.assertEqual(day10.part2(input), 8)
+
+    def test_part2_on_example_b(self):
+        input_file = os.path.abspath(os.path.dirname(__file__)) + os.path.sep + "example10b.txt"
+        with open(input_file) as f:
+            input = [l.strip() for l in f]
+
+        self.assertEqual(day10.part2(input), 19208)
 
 if __name__ == "__main__": 
     unittest.main()
