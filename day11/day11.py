3bdcd586b4b89431fe5e4f7dcfbe767e1d6b014b
Markus Bauer <mkbauer@web.de>
2020-12-11 09:07:57 +0100

    Initial solution for day 11 part 1.

--- day11/day11.py
+++ day11/day11.py
@@ -4,9 +4,94 @@ import re
 import os
 import sys
 
+from typing import List
+
+OCCUPIED = "#"
+EMPTY = "L"
+
+class Game:
+
+    def __init__(self, lines: List[str]):
+        self.cells : List[str] = []
+        self.y_max = len(lines)-1
+        self.x_max = len(lines[0])-1
+        for line in lines:
+            self.cells.append(line)
+    
+    def next_generation(self) -> bool:
+        changed = False
+        number_of_neighbours : List[List[int]]= []
+        for y in range(0, len(self.cells)):
+            line : List[int] = []
+            for x in range(0, len(self.cells[y])):
+                line.append(self.count_neighbours(x, y))
+            number_of_neighbours.append(line)
+        for y in range(0, len(self.cells)):
+            for x in range(0, len(self.cells[y])):
+                c = number_of_neighbours[y][x]
+                if self.get_cell(x, y) == OCCUPIED and c >= 4:
+                    self.set_cell(x, y, EMPTY)
+                    changed = True
+                if self.get_cell(x, y) == EMPTY and c == 0:
+                    self.set_cell(x, y, OCCUPIED)
+                    changed = True 
+        return changed
+
+    def get_cell(self, x: int, y: int)  -> str:
+        return self.cells[y][x]       
+
+    def set_cell(self, x: int, y: int, value: str):
+        self.cells[y] = self.cells[y][:x] + value + self.cells[y][x+1:]
+
+    def count_neighbours(self, x: int, y: int) -> int:
+        result = 0
+        # Left upper neighbour
+        if x-1 >= 0 and y-1 >= 0 and self.get_cell(x-1, y-1) == OCCUPIED:
+            result += 1
+        # Middle upper neighbour
+        if y-1 >= 0 and self.get_cell(x, y-1) == OCCUPIED: 
+            result += 1
+        # Right upper neighbour
+        if x+1 <= self.x_max and y-1 >= 0 and self.get_cell(x+1, y-1) == OCCUPIED: 
+            result += 1
+        # Left neigbour
+        if x-1 >= 0 and self.get_cell(x-1, y) == OCCUPIED:
+            result += 1
+        # Right neighbour
+        if x+1 <= self.x_max and self.get_cell(x+1, y) == OCCUPIED:
+            result += 1
+        # Lower left neighbour
+        if x-1 >= 0 and y+1 <= self.y_max and self.get_cell(x-1, y+1) == OCCUPIED:
+            result += 1
+        # Lower neighbour
+        if y+1 <= self.y_max and self.get_cell(x, y+1) == OCCUPIED:
+            result += 1
+        # Lower right neighbour
+        if y+1 <= self.y_max and x+1 <= self.x_max and self. get_cell(x+1, y+1) == OCCUPIED:
+            result += 1
+        return result
+
+    def count_occupied_seats(self):
+        result = 0
+        for line in self.cells:
+            result += line.count(OCCUPIED)
+        return result
+
+    def print(self):
+        for line in self.cells:
+            print(line)
+
 def part1(input):
-    result = 0
+    game = Game(input)
+    gens = 0
+    while game.next_generation():
+        gens += 1
+        game.print()
+        print()
+    print("Stable after %d generations." % gens)
+    result = game.count_occupied_seats()
     return result
+
 
 def part2(input):
     result = 0
--- day11/day11_test.py
+++ day11/day11_test.py
@@ -4,19 +4,33 @@ import unittest
 import os
 import sys
 
-import day11
+from day11 import Game
+from day11 import part1
+from day11 import part2
 
 class Day11Test(unittest.TestCase):
 
+    def test_count_neighbours(self):
+        game = Game(["#.##.##.##", "#######.##", "#.#.#..#.."])
+        self.assertEqual(game.count_neighbours(1, 1), 6)
+        self.assertEqual(game.count_neighbours(0, 0), 2)
+        self.assertEqual(game.count_neighbours(1, 0), 5)
+        self.assertEqual(game.count_neighbours(0, 1), 3)
+        self.assertEqual(game.count_neighbours(9, 0), 3)
+
+    def test_set_cell(self):
+        game = Game(["#.##.##.##", "#######.##", "#.#.#..#.."])
+        game.set_cell(1, 1, "L")
+        self.assertEqual(game.cells, ["#.##.##.##", "#L#####.##", "#.#.#..#.."])
     def test_part1_on_example_data(self):
         input_file = os.path.abspath(os.path.dirname(__file__)) + os.path.sep + "example11.txt"
         with open(input_file) as f:
             input = [l.strip() for l in f]
-        self.assertEqual(day11.part1(input), 0)
+        self.assertEqual(part1(input), 37)
 
     def test_part2(self):
         input = [""]
-        self.assertEqual(day11.part2(input), 0)
+        self.assertEqual(part2(input), 0)
 
 if __name__ == "__main__": 
     unittest.main()
