83813570e4c375ab29f87fa97c3755b5aa777c33
Markus Bauer <mkbauer@web.de>
2020-12-12 13:52:36 +0100

    Solution for day 12, part 2

--- day12/day12.py
+++ day12/day12.py
@@ -5,6 +5,46 @@ import os
 import sys
 import math
 from typing import List
+from typing import Tuple
+
+# Rotate a point around the origin (by an aritrary angle)
+def rotate_point(point: Tuple[int, int], angle: int) -> Tuple[int, int]:
+    (x, y) = point
+    # Convert point into polar coordinates... 
+    # Special cases
+    if x == 0:
+        if y > 0:
+            alpha = 0.5 * math.pi
+        else:
+            alpha = 1.5 * math.pi 
+    else:
+        alpha = math.atan2(float(y), float(x)) 
+    distance = math.sqrt(x*x + y*y)
+    # ...add the angle...
+    new_alpha = alpha + math.radians(angle) # or: new_alpha = alpha + (float(angle) * math.pi / 180)
+    # ...and convert it back to cartesian coordinates
+    x = round(math.cos(new_alpha) * distance)
+    y = round(math.sin(new_alpha) * distance)
+    return (x, y)
+
+# Rotate a point around the origin (by fixed angles 0, 90, 180, 270,...)
+def rotate_point_by_90s(point: Tuple[int, int], angle: int) -> Tuple[int, int]:
+    (x, y) = point
+    # Make shure angle is between 0..360
+    my_angle = math.fmod(angle, 360)
+    if my_angle < 0:
+        my_angle = 360 + my_angle
+    if my_angle not in [0, 90, 180, 270]:
+        msg = "Illegal rotation angle: {angle}!"
+        raise ValueError(msg.format(angle=angle))
+    if my_angle == 90:
+        (x, y) = (-y, x)
+    if my_angle == 180:
+        (x, y) = (-x, -y)
+    if my_angle == 270:
+        (x, y) = (y, -x)
+    return (x, y)
+    
 
 class Ship:
 
@@ -12,33 +52,73 @@ class Ship:
         self.x = x
         self.y = y
         self.orientation = orientation
+        self.use_waypoint = False
+        self.wp_x = 0
+        self.wp_y = 0
+
+    # This switches the ship into waypoint navigation, see part 2
+    def turnon_waypoint(self, x: int, y: int):
+        self.use_waypoint = True
+        self.wp_x = x
+        self.wp_y = y
 
     def go_north(self, distance: int):
-        self.y += distance
+        if self.use_waypoint:
+            self.wp_y += distance
+        else:
+            self.y += distance
 
     def go_south(self, distance: int):
-        self.y -= distance
+        if self.use_waypoint:
+            self.wp_y -= distance
+        else:
+            self.y -= distance
 
     def go_east(self, distance: int):
-        self.x += distance
+        if self.use_waypoint:
+            self.wp_x += distance
+        else:
+            self.x += distance
 
     def go_west(self, distance: int):
-        self.x -= distance
+        if self.use_waypoint:
+            self.wp_x -= distance
+        else:
+            self.x -= distance
 
     def turn_left(self, degrees: int):  
-        self.orientation += degrees
+        if self.use_waypoint:
+            p1 = rotate_point((self.wp_x, self.wp_y), degrees)
+            p2 = rotate_point_by_90s((self.wp_x, self.wp_y), degrees)
+            if p1 != p2:
+                msg = "Left rotation of ({x}, {y}) by {angle} failed!" 
+                raise ValueError(msg.format(x=self.wp_x, y=self.wp_y, angle=degrees))
+            (self.wp_x, self.wp_y) = p1
+        else:    
+            self.orientation += degrees
 
     def turn_right(self, degrees: int):
-        self.orientation -= degrees
+        if self.use_waypoint:
+            p1 = rotate_point((self.wp_x, self.wp_y), -degrees)
+            p2 = rotate_point_by_90s((self.wp_x, self.wp_y), -degrees)
+            if p1 != p2:
+                msg = "Right rotation of ({x}, {y}) by {angle} failed!" 
+                raise ValueError(msg.format(x=self.wp_x, y=self.wp_y, angle=degrees))
+            (self.wp_x, self.wp_y) = p1
+        else:    
+            self.orientation -= degrees
 
     def go_forward(self, distance: int):
-        rad = float(self.orientation) * math.pi / 180
-        dx = round(math.cos(rad) * distance)
-        dy = round(math.sin(rad) * distance)
+        if self.use_waypoint:
+            dx = self.wp_x * distance
+            dy = self.wp_y * distance
+        else:
+            rad = math.radians(float(self.orientation)) # rad = float(self.orientation) * math.pi / 180
+            dx = round(math.cos(rad) * distance)
+            dy = round(math.sin(rad) * distance)
         self.x += dx
         self.y += dy
-        print("go_forward by (%d, %d) to (%d, %d)" % (dx, dy, self.x, self.y))
-
+        # print("go_forward by (%d, %d) to (%d, %d)" % (dx, dy, self.x, self.y))
 
     def compute_distance(self):
         return abs(self.x) + abs(self.y)
@@ -47,7 +127,7 @@ class Ship:
         for instruction in input:
             cmd = instruction[0]
             value = int(instruction[1:])
-            print("%s %d" % (cmd, value))
+            # print("%s %d" % (cmd, value))
             if cmd == "N":
                 self.go_north(value)
             if cmd == "S":
@@ -65,17 +145,26 @@ class Ship:
             self.print()
     
     def print(self):
-        print("Ship: (%d %d), orientation: %d" % (self.x, self.y, self.orientation))
+        if self.use_waypoint:
+            print("Ship: (%d %d), with waypoint at relative (%d, %d)" 
+                % (self.x, self.y, self.wp_x, self.wp_y))
+        else:
+            print("Ship: (%d %d), orientation: %d" % (self.x, self.y, self.orientation))
 
             
 def part1(input):
     ship = Ship()
+    ship.print()
     ship.navigate(input)
     result = ship.compute_distance()
     return result
 
 def part2(input):
-    result = 0
+    ship = Ship()
+    ship.turnon_waypoint(10, 1)
+    ship.print()
+    ship.navigate(input)
+    result = ship.compute_distance()
     return result
 
 def main():    
--- day12/day12_test.py
+++ day12/day12_test.py
@@ -5,12 +5,15 @@ import os
 import sys
 
 from day12 import Ship
+from day12 import rotate_point
+from day12 import rotate_point_by_90s
 
 class Day12Test(unittest.TestCase):
 
     def test_part1(self):
         input = ["F10", "N3", "F7", "R90", "F11"]
         ship = Ship()
+        ship.print()
         ship.navigate(input)
         self.assertEqual(ship.x, 17)
         self.assertEqual(ship.y, -8)
@@ -24,18 +27,53 @@ class Day12Test(unittest.TestCase):
         self.assertEqual(ship.y, -8)
         self.assertEqual(ship.compute_distance(), 25)
 
-
-
-
-    # def test_part1_on_example_data(self):
-    #     input_file = os.path.abspath(os.path.dirname(__file__)) + os.path.sep + "example12.txt"
-    #     with open(input_file) as f:
-    #         input = [l.strip() for l in f]
-    #     self.assertEqual(day12.part1(input), 0)
-
     def test_part2(self):
-        input = [""]
-        # self.assertEqual(day12.part2(input), 0)
+        input = ["F10", "N3", "F7", "R90", "F11"]
+        ship = Ship()
+        ship.turnon_waypoint(10, 1)
+        ship.print()
+        ship.navigate(input)
+        self.assertEqual(ship.x, 214)
+        self.assertEqual(ship.y, -72)
+        self.assertEqual(ship.compute_distance(), 286)
 
+    def test_part2_turn_left(self):
+        input = ["F10", "L180", "F10"]
+        ship = Ship()
+        ship.turnon_waypoint(10, 1)
+        ship.print()
+        ship.navigate(input)
+        self.assertEqual(ship.x, 0)
+        self.assertEqual(ship.y, 0)
+
+    def test_rotate_waypoint(self):
+        ship = Ship(170, 38)
+        ship.turnon_waypoint(10, 4)
+        ship.turn_right(90)
+        self.assertEqual(ship.x, 170)
+        self.assertEqual(ship.y, 38)
+        self.assertEqual(ship.wp_x, 4)
+        self.assertEqual(ship.wp_y, -10)
+        ship.go_forward(11)
+        self.assertEqual(ship.x, 214)
+        self.assertEqual(ship.y, -72)
+        self.assertEqual(ship.compute_distance(), 286)
+
+    def test_rotate_point(self):
+        p1 = (-2, 15)
+        self.assertEqual(rotate_point(p1, -90), (15, 2))
+        self.assertEqual(rotate_point_by_90s(p1, -90), (15, 2))
+
+
+    def test_rotate_waypoint_special_cases(self):
+        ship = Ship(0, 0)
+        ship.turnon_waypoint(1, 0)
+        ship.turn_left(90)
+        self.assertEqual(ship.wp_x, 0)
+        self.assertEqual(ship.wp_y, 1)
+        ship.turn_left(90)
+        self.assertEqual(ship.wp_x, -1)
+        self.assertEqual(ship.wp_y, 0)
+ 
 if __name__ == "__main__": 
     unittest.main()
