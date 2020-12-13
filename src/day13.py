a78f0d13dca0a42029b048db405afe696bfbcee6
Markus Bauer <mkbauer@web.de>
2020-12-13 10:23:47 +0100

    Day 14: Solution for part 1 + naive solution for part 2

--- src/day13.py
+++ src/day13.py
@@ -1,26 +1,70 @@
 #!/usr/bin/env python3
 
+import logging
 import re
 import os
 import sys
 
+from typing import List, Tuple
+
 from utils import read_inputfile
 
+def buses_in_service(s: str) -> List[int]:
+    buses = s.split(",")
+    return [ int(bus) for bus in buses if bus != "x"]
+
+def find_next_bus(time: int, buses: List[int]) -> Tuple[int, int]:
+    next_bus = 0
+    t = time
+    while next_bus == 0:
+        for bus in buses:
+            if t % bus == 0:
+                next_bus = bus
+                break
+        t += 1
+    return (next_bus, t-1)
+
 def part1(input):
-    result = 0
+    time = int(input[0])
+    buses = buses_in_service(input[1])
+    (next_bus, departure_time) = find_next_bus(time, buses)
+    result = (departure_time-time) * next_bus
     return result
 
-def part2(input):
-    result = 0
-    return result
-
+def part2(s: str):
+    buses = s.split(",")
+    offsets: List[Tuple[int, int]] = []
+    offset = 0
+    highest_bus_number = 0
+    offset_of_highest_bus = 0
+    for bus in buses:
+        if bus != "x":
+            bus_number = int(bus)
+            offsets.append((offset, bus_number))
+            if (bus_number > highest_bus_number):
+                offset_of_highest_bus = offset
+                highest_bus_number = bus_number
+        offset += 1  
+    i = 1
+    while True:
+        time = i * highest_bus_number - offset_of_highest_bus 
+        # print(time) 
+        do_all_buses_match = True
+        for (offset, bus_number) in offsets:
+            if not (time + offset) % bus_number == 0:
+                do_all_buses_match = False
+                break
+        if do_all_buses_match:
+            return time
+        i += 1
+    
 def main():    
 
     # Official input
     input = read_inputfile("input13.txt")
 
     print("The solution for part 1 on the official input is %d" % (part1(input)))
-    print("The solution for part 2 on the official input is %d" % (part2(input)))
+    print("The solution for part 2 on the official input is %d" % (part2(input[1])))
 
 if __name__ == "__main__": 
     main()
--- tests/day13_test.py
+++ tests/day13_test.py
@@ -4,19 +4,35 @@ import unittest
 import os
 import sys
 
-from day13 import part1
-from day13 import part2
+from day13 import buses_in_service, part1, part2
 from utils import read_inputfile
 
 class Day13Test(unittest.TestCase):
 
+    def test_buses_in_service(self):
+        input = "7,13,x,x,59,x,31,19"
+        buses = buses_in_service(input)
+        self.assertEqual(sorted(buses), [7, 13, 19, 31, 59])
+
     def test_part1_on_example_data(self):
         input = read_inputfile("example13.txt")
-        self.assertEqual(part1(input), 0)
+        self.assertEqual(part1(input), 295)
 
     def test_part2(self):
-        input = [""]
-        self.assertEqual(part2(input), 0)
+        s = "17,x,13,19"
+        self.assertEqual(part2(s), 3417)
+        s = "67,7,59,61"
+        self.assertEqual(part2(s), 754018)
+        s = "67,x,7,59,61"
+        self.assertEqual(part2(s), 779210)
+        s = "67,7,x,59,61"
+        self.assertEqual(part2(s), 1261476)
+        s = "1789,37,47,1889"
+        self.assertEqual(part2(s), 1202161486)
+
+    def test_part2_on_example_data(self):
+        input = read_inputfile("example13.txt")
+        self.assertEqual(part2(input[1]), 1068781)
 
 if __name__ == "__main__": 
     unittest.main()
