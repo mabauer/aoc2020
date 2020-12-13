22ceecec179b3148abc9f5bda11e722d67524056
Markus Bauer <mkbauer@web.de>
2020-12-13 04:30:30 +0100

    Skeleton for day 13.

---
+++ src/day13.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import re
+import os
+import sys
+
+from utils import read_inputfile
+
+def part1(input):
+    result = 0
+    return result
+
+def part2(input):
+    result = 0
+    return result
+
+def main():    
+
+    # Official input
+    input = read_inputfile("input13.txt")
+
+    print("The solution for part 1 on the official input is %d" % (part1(input)))
+    print("The solution for part 2 on the official input is %d" % (part2(input)))
+
+if __name__ == "__main__": 
+    main()
+
