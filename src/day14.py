b2a777e644036745ad4b133bc941eda64c14d09b
Markus Bauer <mkbauer@web.de>
2020-12-14 07:31:28 +0100

    Solution for day 14, part 1.

--- src/day14.py
+++ src/day14.py
@@ -3,11 +3,47 @@
 import re
 import os
 import sys
+import math
+from typing import Dict
 
 from utils import read_inputfile
 
+def to_binary_str(value : int):
+    result = "{:036b}".format(value)
+    return result
+
+def bitwise_not(n, numbits=36):
+    return (1 << numbits) - 1 - n
+
+def apply_mask(value: int, mask: str):
+    m1 = int(mask.replace("X", "0"), 2)
+    m2 = int(mask.replace("X", "1"), 2)
+    # print("value: %s" % to_binary_str(value))
+    # print("mask: %s" % mask)
+    value = value | m1
+    # print("after m1: %s" % to_binary_str(value))
+    value = value & m2
+    # print("after m2: %s" % to_binary_str(value))
+    return value
+
+def run_instructions_part1(input):
+    mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    mem : Dict[int, int] = {}
+    for line in input:
+        s = line.replace(" ", "")
+        (lhs, rhs) = s.split("=")
+        if lhs == "mask":
+            mask = rhs
+        else:
+            mo = re.search("mem\[([0-9]+)\]", lhs)
+            addr = int(mo.group(1))
+            value = int(rhs)
+            mem[addr] = apply_mask(value, mask)
+    result = sum(mem.values())
+    return result
+        
 def part1(input):
-    result = 0
+    result = run_instructions_part1(input)
     return result
 
 def part2(input):
--- tests/day14_test.py
+++ tests/day14_test.py
@@ -4,14 +4,21 @@ import unittest
 import os
 import sys
 
-from day14 import part1, part2
+from day14 import apply_mask, part1, part2, to_binary_str
 from utils import read_inputfile
 
-class DayXXTest(unittest.TestCase):
+class Day14Test(unittest.TestCase):
+
+    def test_to_binary_str(self):
+        self.assertEqual(to_binary_str(101), "000000000000000000000000000001100101")
+
+    def test_apply_mask(self):
+        mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"
+        self.assertEqual(apply_mask(11, mask), 73)
 
     def test_part1_on_example_data(self):
         input = read_inputfile("example14.txt")
-        self.assertEqual(part1(input), 0)
+        self.assertEqual(part1(input), 165)
 
     def test_part2(self):
         input = [""]
