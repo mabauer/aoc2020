051effe2c2bf910924aba35dc3e3b239c0a040c8
Markus Bauer <mkbauer@web.de>
2020-12-15 08:32:55 +0100

    Day 15: Test cases, small improvements for readability

--- src/day15.py
+++ src/day15.py
@@ -9,15 +9,15 @@ def play(input, turns=2020):
 
     # Play the first rounds based on the input
     last_spoken = 0
-    for turn in range(1, len(input)+1):
-        last_spoken = input[turn-1]
+    for turn in range(1, len(input) + 1):
+        last_spoken = input[turn - 1]
         numbers[last_spoken] = turn
         # print("%d: %d -- %s" % (turn, last_spoken, numbers))
 
     # Now, play the actual game
     # Note: we always consider the last (i.e. the previous) turn!
-    for last_turn in range(len(input), turns):
-        turn = last_turn + 1
+    for turn in range(len(input) + 1, turns + 1):
+        last_turn = turn -1
         # Did we have the last spoken number before?
         if last_spoken in numbers:
             number = last_turn  - numbers[last_spoken]
--- tests/day15_test.py
+++ tests/day15_test.py
@@ -11,10 +11,25 @@ class Day15Test(unittest.TestCase):
         input = [0,3,6]
         self.assertEqual(play(input, 10), 0)
 
-    def test_play(self):
+    def test_play_more_examples(self):
         self.assertEqual(play([1,3,2]), 1)
         self.assertEqual(play([2,1,3]), 10)
+        self.assertEqual(play([1,2,3]), 27)
+        self.assertEqual(play([2,3,1]), 78)
+        self.assertEqual(play([3,2,1]), 438)
+        self.assertEqual(play([3,1,2]), 1836)
 
+    def test_play_examples_part_2(self):
+        # These tests run a long time, to activate set long_running = True
+        long_running = False
+        if long_running:
+            self.assertEqual(play([0,3,6], 30000000), 175594)
+            self.assertEqual(play([1,3,2], 30000000), 2578)
+            self.assertEqual(play([2,1,3], 30000000), 3544142)
+            self.assertEqual(play([1,2,3], 30000000), 261214)
+            self.assertEqual(play([2,3,1], 30000000), 6895259)
+            self.assertEqual(play([3,2,1], 30000000), 18)
+            self.assertEqual(play([3,1,2], 30000000), 362)
 
 if __name__ == "__main__": 
     unittest.main()
