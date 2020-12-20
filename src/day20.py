741525cc36793fedcffa9ad92be038ccbfa8eafa
Markus Bauer <mkbauer@web.de>
2020-12-20 21:15:32 +0100

    Day 20: Finally, a solution for part 2.

--- src/day20.py
+++ src/day20.py
@@ -1,20 +1,155 @@
 #!/usr/bin/env python3
-
+from __future__ import annotations
+from enum import IntEnum
 import re
 
-from typing import Dict, List
-from itertools import permutations
+
+from typing import Dict, List, Tuple, Optional
 
 from utils import read_inputfile
 
-Tile = List[str]
-Edges = List[str]
+EdgeSet = List[str]
+
+class Orientation(IntEnum):
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3 
+
+class Tile:
+
+    def __init__(self, id, lines: List[str]):
+        self.cells : List[str] = lines
+        self.id = id
+
+    def __eq__(self, other):
+        return self.cells == other.cells
+
+    def __repr__(self):
+        return str(self.id) + "\n" + "\n".join(self.cells)
+
+    def __hash__(self):
+        return hash("".join(self.cells))
+
+    def get_edges(self) -> EdgeSet:
+        edges : EdgeSet = []
+        top = self.cells[0]
+        left = ""
+        right = ""
+        for line in self.cells:
+            left += line[0]
+            right += line[len(line)-1]
+        bottom = self.cells[len(self.cells)-1]
+        # bottom = bottom[::-1]
+        # left = left[::-1]
+        edges = [top, right, bottom, left]
+        return edges
+
+    def count_comon_edges(self, other) -> int:
+        same = 0
+        for e1 in self.get_edges():
+            for e2 in other.get_edges():
+                if e1 == e2 or e1 == e2[::-1]:
+                    same += 1
+        return same
+
+    def rotate(self):
+        rotated = [ ("".join(x))[::-1] for x in zip(*self.cells) ]
+        return Tile(self.id, rotated)
+
+    def flip_vert(self):
+        flipped = [ line[::-1] for line in self.cells ]
+        return Tile(self.id, flipped)
+
+    def flip_horiz(self):
+        flipped = self.cells[::-1]
+        return Tile(self.id, flipped)
+
+    def get_variants(self):
+        result = []
+        rotated = self
+        for i in range(0, 4):
+            result.append(rotated)
+            result.append(rotated.flip_vert())
+            result.append(rotated.flip_horiz())
+            rotated = rotated.rotate()
+        return result
+
+    def get_neighbour(self, o, tiles: Dict[int, Tile]) -> Optional[Tile]:
+        neighbours : List[int] = []
+        for t in tiles:
+            tile = tiles[t]
+            if tile.id != self.id:
+                if self.count_comon_edges(tile) > 0:
+                    neighbours.append(t)
+        # print(neighbours)   
+        result = None
+        for n in neighbours:
+            neighbour = tiles[n]
+            variants = neighbour.get_variants()
+            if o == Orientation.UP:
+                for variant in variants:
+                    if variant.get_edges()[Orientation.DOWN] == self.get_edges()[o]:
+                        result = variant
+            if o == Orientation.DOWN:
+                for variant in variants:
+                    if variant.get_edges()[Orientation.UP] == self.get_edges()[o]:
+                        result = variant
+            if o == Orientation.LEFT:
+                for variant in variants:
+                    if variant.get_edges()[Orientation.RIGHT] == self.get_edges()[o]:
+                        result = variant
+            if o == Orientation.RIGHT:
+                for variant in variants:
+                    if variant.get_edges()[Orientation.LEFT] == self.get_edges()[o]:
+                        result = variant
+        # print(result)
+        return result 
+
+    def find_sea_monsters(self):
+        pattern1 = "([#\s]{18}#[#\s])"
+        pattern2 = "(#[#\s]{4}##[#\s]{4}##[#\s]{4}###)"
+        pattern3 = "([#\s]#[#\s]{2}#[#\s]{2}#[#\s]{2}#[#\s]{2}#[#\s]{2}#[#\s]{3})"
+        monster1 = "                  O "
+        monster2 = "O    OO    OO    OOO"
+        monster3 = " O  O  O  O  O  O   "
+        monsters = 0
+        lines = [ line.replace(".", " ") for line in self.cells ]
+        for i in range(1, len(lines)-1):
+            for mo2 in re.finditer(pattern2, lines[i]):
+                x2 = mo2.start()
+                mo3 = re.search(pattern3, lines[i+1][x2:])
+                if mo3 is not None:
+                    x3 = mo3.start()
+                    mo1 = re.search(pattern1, lines[i-1][x2:])
+                    if mo1 is not None:
+                        x1 = mo1.start()
+                        if x1 == 0 and x3 == 0:
+                            monsters += 1 
+                            lines[i] = lines[i][:x2] + monster2 + lines[i][x2+20:] 
+                            lines[i-1] = lines[i-1][:x2] + monster1 + lines[i-1][x2+20:] 
+                            lines[i+1] = lines[i+1][:x2] + monster3 + lines[i+1][x2+20:]
+        for i in range(0, len(lines)):
+            line = lines[i]
+            for pos in range(0, len(line)):
+                if line[pos] == "O":
+                    self.cells[i] = self.cells[i][:pos] + "O" + self.cells[i][pos+1:]
+        return monsters
+
+    def count_hashes(self):
+        result = 0
+        for line in self.cells:
+            for ch in line:
+                if ch == "#":
+                    result += 1
+        return result
+        
 
 def parse_tiles(input) -> Dict[int, Tile]:
     l= 0
     in_tile = False
     tiles = {}
-    tile : Tile = []
+    tile : List[str] = []
     no = 0
     while l < len(input):
         line = input[l]
@@ -28,66 +163,87 @@ def parse_tiles(input) -> Dict[int, Tile]:
                 tile.append(line)
             else:
                 in_tile = False
-                tiles[no] = tile
+                tiles[no] = Tile(no, tile)
         l += 1
     if in_tile:
-        tiles[no] = tile
+        tiles[no] = Tile(no, tile)
     return(tiles)
 
-def compute_edges(tiles: Dict[int, Tile]) -> Dict[int, Edges]:
-    result : Dict[int, Edges] = {}
-    for no in tiles:
-        tile = tiles[no]
-        edges : Edges = []
-        top = tile[0]
-        left = ""
-        right = ""
-        for line in tile:
-            left += line[0]
-            right += line[len(line)-1]
-        bottom = tile[len(tile)-1]
-        # bottom = bottom[::-1]
-        # left = left[::-1]
-        edges = [top, right, bottom, left]
-        result[no] = edges
+
+def find_corners(tiles: Dict[int, Tile]) -> List[int]:
+    corners = []
+    for t1 in tiles:
+        sum = 0
+        for t2 in tiles:
+            if t1 != t2:
+                same = tiles[t1].count_comon_edges(tiles[t2])
+                sum += same                   
+        # print("%d has %d neighbours" % (t1, sum))
+        if sum == 2:
+            corners.append(t1)
+        assert sum <= 4
+    # print(corners)
+    return corners
+
+
+def compute_image(tiles: Dict[int, Tile]):
+    image : List[List[Tile]]= []
+    corners = find_corners(tiles)
+    c = corners[0]
+    corner = tiles[c] 
+    right = None
+    down = None
+    for variant in corner.get_variants():
+        right = variant.get_neighbour(Orientation.RIGHT, tiles)
+        down = variant.get_neighbour(Orientation.DOWN, tiles)  
+        if right is not None and down is not None:
+            down = variant
+            # print("Found left, upper corner!")
+            break
+    j = 0
+    while down is not None:
+        row = []
+        i = 0
+        right = down
+        while right is not None:
+            # print("(%d, %d) = %d" % (j, i, right.id))
+            row.append(right)
+            right = right.get_neighbour(Orientation.RIGHT, tiles)
+            i += 1
+
+        down = down.get_neighbour(Orientation.DOWN, tiles)
+        image.append(row)
+        j += 1
+    size = len(image[0][0].cells)
+    lines : List[str] = []
+    for y in image:
+        for i in range(1, size-1):
+            line = ""
+            for x in y:
+                line += x.cells[i][1:size-1]
+            lines.append(line)
+    result = Tile(0, lines)
+    # print(result)
     return result
 
-# def rotate_tile(edge_set: Edges, pos: int) -> Edges:
-#     return edge_set[pos:] + edge_set[0:pos]
-
-def count_comon_edges(edge_set1: Edges, edge_set2: Edges):
-    same = 0
-    for e1 in edge_set1:
-        for e2 in edge_set2:
-            if e1 == e2 or e1 == e2[::-1]:
-                same += 1
-    return same
-
-def find_corners(edges: Dict[int, Edges]) -> List[int]:
-    corners = []
-    for es1 in edges:
-        sum = 0
-        for es2 in edges:
-            if es1 != es2:
-                same = count_comon_edges(edges[es1], edges[es2])
-                sum += same                   
-        # print("%d has %d neighbours" % (es1, sum))
-        if sum == 2:
-            corners.append(es1)
-    print(corners)
-    return corners
 
 def part1(input):
     tiles = parse_tiles(input)
-    edges = compute_edges(tiles)
-    corners = find_corners(edges)
+    corners = find_corners(tiles)
     result = 1
     for corner in corners:
         result = result * corner
     return result
 
 def part2(input):
+    tiles = parse_tiles(input)
+    sea = compute_image(tiles)
     result = 0
+    for variant in set(sea.get_variants()):
+        found = variant.find_sea_monsters()
+        if found > 0:
+            print(variant)
+            result = variant.count_hashes()
     return result
 
 def main():    
--- tests/day20_test.py
+++ tests/day20_test.py
@@ -4,7 +4,7 @@ import unittest
 import os
 import sys
 
-from day20 import compute_edges, count_comon_edges, parse_tiles, part1, part2, find_corners
+from day20 import Orientation, Tile, compute_image, parse_tiles, part1, part2, find_corners
 from utils import read_inputfile
 
 class Day20Test(unittest.TestCase):
@@ -12,42 +12,79 @@ class Day20Test(unittest.TestCase):
     def test_parse_tiles_on_example_data(self):
         input = read_inputfile("example20.txt")
         tiles = parse_tiles(input)
-        print(tiles.keys())
+        # print(tiles.keys())
         self.assertEqual(len(tiles), 9)
 
     def test_compute_edges_on_example_data(self):
         input = read_inputfile("example20.txt")
         tiles = parse_tiles(input)
-        edges = compute_edges(tiles)
-        self.assertEqual(len(edges), 9)
-        print(edges.keys())
-        self.assertEqual(edges[3079][0], "#.#.#####.")
-        self.assertEqual(edges[3079][3], "#..##.#...")
-        # self.assertEqual(edges[3079][3], "...#.##..#")
-
-    # def test_rotate_tile(self):
-    #     edge_set = ["1", "2", "3", "4"]
-    #     self.assertEqual(rotate_tile(edge_set, 2), ["3", "4", "1", "2"])
+        edgeset = tiles[3079].get_edges()
+        self.assertEqual(edgeset[Orientation.UP], "#.#.#####.")
+        self.assertEqual(edgeset[Orientation.LEFT], "#..##.#...")
 
     def test_count_common_edges(self):
         input = read_inputfile("example20.txt")
         tiles = parse_tiles(input)
-        edges = compute_edges(tiles)
-        print("Common edges: %s" % count_comon_edges(edges[2971], edges[1489]))
+        self.assertEqual(tiles[2971].count_comon_edges(tiles[1489]), 1)
+
+    def test_rotate(self):
+        tile = Tile(0, ["123", "456", "789"])
+        rotated_tile = tile.rotate()
+        # print(rotated_tile)
+        self.assertEqual(rotated_tile.cells, ['741', '852', '963'])
+
+    def test_flip_vert(self):
+        tile = Tile(0, ["123", "456", "789"])
+        flipped_tile = tile.flip_vert()
+        print(flipped_tile.cells)
+        self.assertEqual(flipped_tile.cells, ['321', '654','987'])
+        self.assertEqual(flipped_tile.flip_vert(), tile)
+
+    def test_flip_horiz(self):
+        tile = Tile(0, ["123", "456", "789"])
+        flipped_tile = tile.flip_horiz()
+        print(flipped_tile.cells)
+        self.assertEqual(flipped_tile.cells, ['789', '456', '123'])
+        self.assertEqual(flipped_tile.flip_horiz(), tile)
+
+    def test_get_variants(self):
+        input = read_inputfile("example20.txt")
+        tiles = parse_tiles(input)
+        variants = tiles[3079].get_variants()
+        for tile in variants:
+            print(tile)
+            print()
+        print("Variants: %d, %d unique." % (len(variants), len(set(variants))))
 
     def test_find_corners(self):
         input = read_inputfile("example20.txt")
         tiles = parse_tiles(input)
-        edges = compute_edges(tiles)
-        self.assertEqual(find_corners(edges), [1951, 1171, 2971, 3079])
+        self.assertEqual(find_corners(tiles), [1951, 1171, 2971, 3079])
+
+    def test_compute_image(self):
+        # input = read_inputfile("input20.txt")
+        input = read_inputfile("example20.txt")
+        tiles = parse_tiles(input)
+        image = compute_image(tiles)
+        #print(image)
+        solution = read_inputfile("example20solution.txt")
+        expected = Tile(0, solution)
+        self.assertIn(image, expected.get_variants())
+
+    def test_find_sea_monsters(self):
+        input = read_inputfile("example20b.txt")
+        sea = Tile(0, input)
+        self.assertEqual(sea.find_sea_monsters(), 2)
+        self.assertEqual(sea.count_hashes(), 273)
+        
 
     def test_part1_on_example(self):
         input = read_inputfile("example20.txt")
         self.assertEqual(part1(input), 20899048083289)
 
     def test_part2(self):
-        input = [""]
-        self.assertEqual(part2(input), 0)
+        input = read_inputfile("example20.txt")
+        self.assertEqual(part2(input), 273)
 
 if __name__ == "__main__": 
     unittest.main()
