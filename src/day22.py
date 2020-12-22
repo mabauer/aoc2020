f82d13208d79e973925237acfea3b7bfc9373388
Markus Bauer <mkbauer@web.de>
2020-12-22 13:42:24 +0100

    Solution for day 22, part 2

--- src/day22.py
+++ src/day22.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import re
-from typing import List
+from typing import List, Tuple
 
 from utils import read_inputfile
 
@@ -9,54 +9,143 @@ Deck = List[int]
 
 class Game:
 
-    def __init__(self, decks: List[Deck]):
-        self.player1 = decks[0]
-        self.player2 = decks[1]
+    def __init__(self, decks: List[Deck], debug=True):
+        self.deck_player1 = decks[0]
+        self.deck_player2 = decks[1]
+        self.debug = debug
         self.round = 0
 
-    def play_round(self, debug=True):
+
+    def play_round(self):
         if self.is_finished():
             return
         self.round += 1 
-        card1 = self.player1[0]
-        card2 = self.player2[0]
-        if debug:
+        card1 = self.deck_player1[0]
+        card2 = self.deck_player2[0]
+        if self.debug:
             print("-- Round %s --" % self.round)
-            print("Player 1's deck: %s" % ", ".join([ str(i) for i in self.player1 ]))
-            print("Player 2's deck: %s" % ", ".join([ str(i) for i in self.player2 ]))
+            print("Player 1's deck: %s" % ", ".join([ str(i) for i in self.deck_player1 ]))
+            print("Player 2's deck: %s" % ", ".join([ str(i) for i in self.deck_player2 ]))
             print("Player 1 plays: %d" % card1)
             print("Player 2 plays: %d" % card2)
         if card1 > card2:
-            if debug:
+            if self.debug:
                 print ("Player 1 wins the round!")
-            self.player1 = self.player1[1:] + [card1, card2]
-            self.player2 = self.player2[1:]
+            self.deck_player1 = self.deck_player1[1:] + [card1, card2]
+            self.deck_player2 = self.deck_player2[1:]
         else:
-            if debug:
+            if self.debug:
                 print ("Player 2 wins the round!")
-            self.player1 = self.player1[1:]
-            self.player2 = self.player2[1:] + [card2, card1]
+            self.deck_player1 = self.deck_player1[1:]
+            self.deck_player2 = self.deck_player2[1:] + [card2, card1]
   
     def is_finished(self):
-        return self.player1 == [] or self.player2 == []
+        return self.deck_player1 == [] or self.deck_player2 == []
 
-    def play_game(self, debug=True):
+
+    def play(self):
         while not self.is_finished():
-            self.play_round(debug)
-        if debug:
-            print("== Post-game results ==")
-            print("Player 1's deck: %s" % ", ".join([ str(i) for i in self.player1 ]))
-            print("Player 2's deck: %s" % ", ".join([ str(i) for i in self.player2 ]))
-        winners_deck = []
-        if self.player1 == []:
-             winners_deck = self.player2
+            self.play_round()
+        winner = 0
+        if self.deck_player1 == []:
+            winner = 2
         else:
-             winners_deck = self.player1
+            winner = 1
+        if self.debug:
+            print("== Post-game results ==")
+            print("Player 1's deck: %s" % ", ".join([ str(i) for i in self.deck_player1 ]))
+            print("Player 2's deck: %s" % ", ".join([ str(i) for i in self.deck_player2 ]))       
+            print("The winner is player %d!" % winner)
+        return winner
+
+    def compute_score(self):
+        winners_deck = []
+        if self.deck_player1 == []:
+             winners_deck = self.deck_player2
+        else:
+             winners_deck = self.deck_player1
         score = 0
         for i, card in enumerate(reversed(winners_deck), start=1):
             score += i * card
         return score
-            
+
+
+class RecursiveGame(Game):
+
+    game_no = 0
+
+    def __init__(self, decks: List[Deck], debug=True):
+        super().__init__(decks, debug)
+        self.previous_decks : List[Tuple[Deck, Deck]] = []
+        RecursiveGame.game_no += 1
+        self.id = RecursiveGame.game_no
+
+    def play_round(self):
+        if self.is_finished():
+            return
+        self.round += 1 
+        self.previous_decks.append((self.deck_player1.copy(), self.deck_player2.copy()))
+        card1 = self.deck_player1[0]
+        card2 = self.deck_player2[0]
+        if self.debug:
+            print("-- Round %s (Game %s) --" % (self.round, self.id))
+            print("Player 1's deck: %s" % ", ".join([ str(i) for i in self.deck_player1 ]))
+            print("Player 2's deck: %s" % ", ".join([ str(i) for i in self.deck_player2 ]))
+            print("Player 1 plays: %d" % card1)
+            print("Player 2 plays: %d" % card2)
+        self.deck_player1 = self.deck_player1[1:]
+        self.deck_player2 = self.deck_player2[1:]
+        winner = 0
+        if card1 <= len(self.deck_player1) and card2 <= len(self.deck_player2):
+            if self.debug:
+                print("Playing a sub-game to determine the winner...")
+            subgame = RecursiveGame([self.deck_player1[:card1].copy(), self.deck_player2[:card2].copy()]) 
+            winner = subgame.play()
+            if self.debug:
+                print("...anyway, back to game %s." % self.id)
+        elif card1 > card2:
+            winner = 1
+        else:
+            winner = 2
+        if winner == 1:
+            if self.debug:
+                print ("Player 1 wins round %d of game %d" % (self.round, self.id))
+            self.deck_player1 = self.deck_player1 + [card1, card2]
+        else:
+            if self.debug:
+                print ("Player 2 wins round %d of game %d" % (self.round, self.id))
+            self.deck_player2 = self.deck_player2 + [card2, card1]
+  
+    def is_finished(self):
+        if self.deck_player1 == [] or self.deck_player2 == []:
+            return True
+        if (self.deck_player1, self.deck_player2) in self.previous_decks:
+            # We already had these decks => Player 2 looses
+            if self.debug:
+                print("Player 1's deck: %s" % ", ".join([ str(i) for i in self.deck_player1 ]))
+                print("Player 2's deck: %s" % ", ".join([ str(i) for i in self.deck_player2 ]))
+                print("Recursion detected before round %d in game %d => winner is 1 (always)" % (self.round, self.id))
+            self.deck_player2 = []
+            return True
+        return False
+
+    def play(self):
+        while not self.is_finished():
+            self.play_round()
+        winner = 0
+        if self.deck_player1 == []:
+            winner = 2
+        else:
+            winner = 1
+        if self.debug:
+            if self.id == 1:
+                print("== Post-game results ==")
+                print("Player 1's deck: %s" % ", ".join([ str(i) for i in self.deck_player1 ]))
+                print("Player 2's deck: %s" % ", ".join([ str(i) for i in self.deck_player2 ]))
+        
+            print("The winner of game %d is: %d" % (self.id, winner))
+        return winner
+
 def read_decks(input) -> List[Deck]:
     player = 0
     decks : List[Deck] = []
@@ -76,13 +165,17 @@ def read_decks(input) -> List[Deck]:
 
 def part1(input):
     decks = read_decks(input)
-    game = Game(decks)
-    score = game.play_game(debug=True)
+    game = Game(decks, debug=True)
+    game.play()
+    score = game.compute_score()
     return score
 
 def part2(input):
-    result = 0
-    return result
+    decks = read_decks(input)
+    game = RecursiveGame(decks, debug=True)
+    game.play()
+    score = game.compute_score()
+    return score
 
 def main():    
 
--- tests/day22_test.py
+++ tests/day22_test.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from day22 import part1, part2
+from day22 import RecursiveGame, part1, part2
 from utils import read_inputfile
 
 class Day22Test(unittest.TestCase):
@@ -11,9 +11,15 @@ class Day22Test(unittest.TestCase):
         input = read_inputfile("example22.txt")
         self.assertEqual(part1(input), 306)
 
-    def test_part2(self):
-        input = [""]
-        self.assertEqual(part2(input), 0)
+    def test_part2_on_example_data(self):
+        input = read_inputfile("example22.txt")
+        self.assertEqual(part2(input), 291)
+
+    def test_part2_on_infinite_example(self):
+        deck_player1 = [43, 19]
+        deck_player2 = [2, 29, 14]
+        game = RecursiveGame([deck_player1, deck_player2], debug=True)
+        game.play()
 
 if __name__ == "__main__": 
     unittest.main()
